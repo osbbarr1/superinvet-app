42c1d8ce4f3b03a2e6c3744741d1bade3109b07d
oscar baquero <osbbarr1@gmail.com>
2022-05-04 07:00:44 -0500

    resolviendo Problema

--- src/service/userService.js
+++ src/service/userService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export class UserServices{
-    baseUrl= "http://localhost:8080";
+    baseUrl= "http://localhost:8080/api/v1/usuario/all/v1";
 
     getAll(){
         return axios.get(this.baseUrl+ 'all').then(res => res.data);
